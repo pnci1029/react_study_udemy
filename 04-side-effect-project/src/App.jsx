d0c3736a7bcdd703a2b53a7ac568fd88568b444e
chhong <81909140+pnci1029@users.noreply.github.com>
2024-05-16 19:37:57 +0900

    24.05.16 simple way to solve problem

--- 04-side-effect-project/src/App.jsx
+++ 04-side-effect-project/src/App.jsx
@@ -43,6 +43,11 @@ function App() {
       const place = AVAILABLE_PLACES.find((place) => place.id === id);
       return [place, ...prevPickedPlaces];
     });
+
+    const storedId = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+    if (storedId.indexOf(id) === -1) {
+      localStorage.setItem('selectedPlaces', JSON.stringify([id, ...storedId]));
+    }
   }
 
   function handleRemovePlace() {
