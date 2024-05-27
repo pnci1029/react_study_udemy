5d5189c87edc97c78132f299bf849eea9ea81c73
chhong <81909140+pnci1029@users.noreply.github.com>
2024-05-27 20:16:02 +0900

    24.05.27 재재수정 퀴즈

--- 05-react-quiz/src/components/Quiz.tsx
+++ 05-react-quiz/src/components/Quiz.tsx
@@ -1,6 +1,7 @@
 import {useState} from "react";
-import question from '../question.js';
-enum AnswerStatus{
+import Question from '../question.js';
+
+enum AnswerStatus {
     WRONG = "WRONG",
     WAIT = "WAIT",
 }
@@ -14,7 +15,10 @@ export function Quiz() {
     // 정답 선택값.
     const [selectedAnswer, setSelectedAnswer] = useState(7);
 
-    const [isAnswered, setIsAnswered]= useState(AnswerStatus.WAIT);
+    const [isAnswered, setIsAnswered] = useState(AnswerStatus.WAIT);
+
+    const [question, setQuestion] = useState([]);
+    const activeQuestionIdx = question.length;
 
     // 핸들러를 커링으로 정의하여 idx값 처리
     const handleAnswerChange = (idx) => (event) => {
@@ -32,41 +36,61 @@ export function Quiz() {
         }
     };
 
-    return (
-        <>
-            <h1>Quiz</h1>
-            {/* 질문목록들 호출 */}
-            {question.map((q, index) => (
-                // 전체 질문 노출이 아닌 인덱스 순서대로 질문 호출
-                currentQuestion === index && (
-                    <div key={q.id}>
-                        <p>{q.text}</p>
-                        <ul>
-                            {/* 선택지가 많기 떄문에 호출 */}
-                            {q.answers.map((answer, idx) => (
-                                // 태그에 정답 부여
-                                <li key={idx}>
-                                    <label>
-                                        <input
-                                            type="radio"
-                                            name="answer"
-                                            value={answer}
-                                            checked={selectedAnswer === idx}
-                                            onChange={handleAnswerChange(idx)}
-                                        />
-                                        {answer}
-                                    </label>
-                                </li>
+    // 버튼 클릭 시 정담 체크하는 함수
+    function handleClickedAnswer(answer) {
+        setQuestion((prev) =>{
+            return [...prev, answer];
+        })
+    }
+    console.log(question)
 
-                            ))}
-                            {isAnswered === AnswerStatus.WRONG ? (
-                                <p style={{color:"red"}}>틀렸습니다 다시 선택하세요 씨발년아</p>
-                            ) : (<></>)}
-                            <button onClick={handleNextQuestion}>Next Question</button>
-                        </ul>
-                    </div>
-                )
-            ))}
-        </>
+    return (
+        <div className={"question"}>
+            <h2>{Question[activeQuestionIdx].text}</h2>
+            <ul>
+                {Question[activeQuestionIdx].answers.map((answer) =>
+                    <li key={answer} className={"answer"}>
+                        <button onClick={() => handleClickedAnswer(answer)}>{answer}</button>
+                    </li>
+                )}
+                {isAnswered === AnswerStatus.WRONG ? (
+                    <p style={{color: "red"}}>틀렸습니다 다시 선택하세요 씨발년아</p>
+                ) : (<></>)}
+            </ul>
+
+            {/*<h1>Quiz</h1>*/}
+            {/*/!* 질문목록들 호출 *!/*/}
+            {/*{question.map((q, index) => (*/}
+            {/*    // 전체 질문 노출이 아닌 인덱스 순서대로 질문 호출*/}
+            {/*    currentQuestion === index && (*/}
+            {/*        <div key={q.id}>*/}
+            {/*            <p>{q.text}</p>*/}
+            {/*            <ul>*/}
+            {/*                /!* 선택지가 많기 떄문에 호출 *!/*/}
+            {/*                {q.answers.map((answer, idx) => (*/}
+            {/*                    // 태그에 정답 부여*/}
+            {/*                    <li key={idx}>*/}
+            {/*                        <label>*/}
+            {/*                            <input*/}
+            {/*                                type="radio"*/}
+            {/*                                name="answer"*/}
+            {/*                                value={answer}*/}
+            {/*                                checked={selectedAnswer === idx}*/}
+            {/*                                onChange={handleAnswerChange(idx)}*/}
+            {/*                            />*/}
+            {/*                            {answer}*/}
+            {/*                        </label>*/}
+            {/*                    </li>*/}
+
+            {/*                ))}*/}
+            {/*                {isAnswered === AnswerStatus.WRONG ? (*/}
+            {/*                    <p style={{color:"red"}}>틀렸습니다 다시 선택하세요 씨발년아</p>*/}
+            {/*                ) : (<></>)}*/}
+            {/*                <button onClick={handleNextQuestion}>Next Question</button>*/}
+            {/*            </ul>*/}
+            {/*        </div>*/}
+            {/*    )*/}
+            {/*))}*/}
+        </div>
     );
 }
