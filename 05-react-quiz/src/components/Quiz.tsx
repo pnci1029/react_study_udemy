88c9adcb9f8fc9bdc8490f7974cf1dc3518d8955
chhong <81909140+pnci1029@users.noreply.github.com>
2024-05-28 20:15:41 +0900

    24.05.28 질문지 셔플 추가

--- 05-react-quiz/src/components/Quiz.tsx
+++ 05-react-quiz/src/components/Quiz.tsx
@@ -12,6 +12,20 @@ export function Quiz() {
     const [question, setQuestion] = useState([]);
     const activeQuestionIdx = question.length;
 
+    /**
+     * 정답을 섞어주기 위해 Question에 있는 answer들을 배열에 삽입
+     * 원본은 그대로 두고 shuffle하기 위해 shuffledAnswer이라는 배열 생성
+     */
+    const shuffledAnswer = [...Question[activeQuestionIdx].answers];
+    console.log(shuffledAnswer)
+
+    /**
+     * 셔플 해주는 코드
+     * 위 콘솔과 아래 콘솔 내용은 다르다.
+     */
+    shuffledAnswer.sort(() =>Math.random() - 0.5);
+    console.log(shuffledAnswer)
+
     // 버튼 클릭 시 정담 체크하는 함수
     function handleClickedAnswer(answer) {
         setQuestion((prev) => {
@@ -26,7 +40,11 @@ export function Quiz() {
             <div id={"question"}>
                 <h2>{Question[activeQuestionIdx].text}</h2>
                 <ul>
-                    {Question[activeQuestionIdx].answers.map((answer) =>
+                    {/*
+                    기존 Question[activeQuestionIdx].answers 구문은 Question Json데이터에서
+                    answer정보를 순차적으로 가져오기 때문에 shuffledAnswer로 교체
+                     */}
+                    {shuffledAnswer.map((answer) =>
                         <li key={answer} className={"answer"}>
                             <button onClick={() => handleClickedAnswer(answer)}>{answer}</button>
                         </li>
