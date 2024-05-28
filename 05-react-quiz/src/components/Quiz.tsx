00a70c8560e832739209d1c0d42f16bde48c4d41
chhong <81909140+pnci1029@users.noreply.github.com>
2024-05-28 20:08:16 +0900

    24.05.28 이전 코드 삭제

--- 05-react-quiz/src/components/Quiz.tsx
+++ 05-react-quiz/src/components/Quiz.tsx
@@ -7,34 +7,10 @@ enum AnswerStatus {
 }
 
 export function Quiz() {
-    // 질문목록을 보여주기위한 상태값. 배열이 0부터 시작이라 초기값 지정
-    const [currentQuestion, setCurrentQuestion] = useState(0);
-    // 정답 상태값.
-    const [currentAnswer, setCurrentAnswer] = useState(1);
-
-    // 정답 선택값.
-    const [selectedAnswer, setSelectedAnswer] = useState(7);
-
     const [isAnswered, setIsAnswered] = useState(AnswerStatus.WAIT);
 
     const [question, setQuestion] = useState([]);
     const activeQuestionIdx = question.length;
-
-    // 핸들러를 커링으로 정의하여 idx값 처리
-    const handleAnswerChange = (idx) => (event) => {
-        // setSelectedAnswer((d) => event.target.value);
-        setSelectedAnswer(idx);
-    };
-
-    const handleNextQuestion = () => {
-        if (selectedAnswer === currentAnswer) {
-            setCurrentQuestion((prevCurrentQuestion) => prevCurrentQuestion + 1);
-            setCurrentAnswer(7);
-            setIsAnswered(AnswerStatus.WAIT);
-        } else {
-            setIsAnswered(AnswerStatus.WRONG)
-        }
-    };
 
     // 버튼 클릭 시 정담 체크하는 함수
     function handleClickedAnswer(answer) {
@@ -59,40 +35,6 @@ export function Quiz() {
                         <p style={{color: "red"}}>틀렸습니다 다시 선택하세요 씨발년아</p>
                     ) : (<></>)}
                 </ul>
-
-                {/*<h1>Quiz</h1>*/}
-                {/*/!* 질문목록들 호출 *!/*/}
-                {/*{question.map((q, index) => (*/}
-                {/*    // 전체 질문 노출이 아닌 인덱스 순서대로 질문 호출*/}
-                {/*    currentQuestion === index && (*/}
-                {/*        <div key={q.id}>*/}
-                {/*            <p>{q.text}</p>*/}
-                {/*            <ul>*/}
-                {/*                /!* 선택지가 많기 떄문에 호출 *!/*/}
-                {/*                {q.answers.map((answer, idx) => (*/}
-                {/*                    // 태그에 정답 부여*/}
-                {/*                    <li key={idx}>*/}
-                {/*                        <label>*/}
-                {/*                            <input*/}
-                {/*                                type="radio"*/}
-                {/*                                name="answer"*/}
-                {/*                                value={answer}*/}
-                {/*                                checked={selectedAnswer === idx}*/}
-                {/*                                onChange={handleAnswerChange(idx)}*/}
-                {/*                            />*/}
-                {/*                            {answer}*/}
-                {/*                        </label>*/}
-                {/*                    </li>*/}
-
-                {/*                ))}*/}
-                {/*                {isAnswered === AnswerStatus.WRONG ? (*/}
-                {/*                    <p style={{color:"red"}}>틀렸습니다 다시 선택하세요 씨발년아</p>*/}
-                {/*                ) : (<></>)}*/}
-                {/*                <button onClick={handleNextQuestion}>Next Question</button>*/}
-                {/*            </ul>*/}
-                {/*        </div>*/}
-                {/*    )*/}
-                {/*))}*/}
             </div>
         </div>
     );
