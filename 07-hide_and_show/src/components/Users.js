4cbeb6f2a6e3a5b48a3a24fa8e19ef00594755c6
chhong <pnci1029@gmail.com>
2024-07-04 19:25:59 +0900

    24.07.04 create pure javascript class component(users)

--- 07-hide_and_show/src/components/Users.js
+++ 07-hide_and_show/src/components/Users.js
@@ -44,29 +44,29 @@ class Users extends Component{
   }
 }
 
-const Users = () => {
-  const [showUsers, setShowUsers] = useState(true);
-
-  const toggleUsersHandler = () => {
-    setShowUsers((curState) => !curState);
-  };
-
-  const usersList = (
-    <ul>
-      {DUMMY_USERS.map((user) => (
-        <User key={user.id} name={user.name} />
-      ))}
-    </ul>
-  );
-
-  return (
-    <div className={classes.users}>
-      <button onClick={toggleUsersHandler}>
-        {showUsers ? 'Hide' : 'Show'} Users
-      </button>
-      {showUsers && usersList}
-    </div>
-  );
-};
+// const Users = () => {
+//   const [showUsers, setShowUsers] = useState(true);
+//
+//   const toggleUsersHandler = () => {
+//     setShowUsers((curState) => !curState);
+//   };
+//
+//   const usersList = (
+//     <ul>
+//       {DUMMY_USERS.map((user) => (
+//         <User key={user.id} name={user.name} />
+//       ))}
+//     </ul>
+//   );
+//
+//   return (
+//     <div className={classes.users}>
+//       <button onClick={toggleUsersHandler}>
+//         {showUsers ? 'Hide' : 'Show'} Users
+//       </button>
+//       {showUsers && usersList}
+//     </div>
+//   );
+// };
 
 export default Users;
