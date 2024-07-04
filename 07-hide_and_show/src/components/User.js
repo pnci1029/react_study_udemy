14bc17111302ad4434d88dc699427c0b13c600ec
chhong <pnci1029@gmail.com>
2024-07-04 19:10:46 +0900

    24.07.04 create pure javascript class component

--- 07-hide_and_show/src/components/User.js
+++ 07-hide_and_show/src/components/User.js
@@ -1,7 +1,23 @@
 import classes from './User.module.css';
+import {Component} from "react";
 
-const User = (props) => {
-  return <li className={classes.user}>{props.name}</li>;
-};
+/**
+ 순수 자바스크립트 클래스
+ 아래와 같은 동작을 하는 클래스
+
+ 다만 함수형이 더 깔끔하고 이 때문에 사람들이 아래를 더 선호함
+ */
+class User extends Component{
+
+  render() {
+    return <li className={classes.user}>{this.props.name}</li>;
+  }
+}
+
+
+
+// const User = (props) => {
+//   return <li className={classes.user}>{props.name}</li>;
+// };
 
 export default User;
