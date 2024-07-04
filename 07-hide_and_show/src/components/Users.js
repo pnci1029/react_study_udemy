e959896d2fdf8f901ce966c29ad5d66059369786
chhong <pnci1029@gmail.com>
2024-07-04 19:25:12 +0900

    24.07.04 create pure javascript class component(users)

--- 07-hide_and_show/src/components/Users.js
+++ 07-hide_and_show/src/components/Users.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import {Component, useState} from 'react';
 import User from './User';
 
 import classes from './Users.module.css';
@@ -8,6 +8,41 @@ const DUMMY_USERS = [
   { id: 'u2', name: 'Manuel' },
   { id: 'u3', name: 'Julie' },
 ];
+
+class Users extends Component{
+  /**
+   함수형 컴포넌트에서는 상태들이 각각의 객체로 관리되나,
+   클래스 컴포넌트는 그렇지 않다.
+
+   클래스 컴포넌트는 선언하려는 모든 상태를 하나의 객체로 만들어야한다.
+   */
+  constructor() {
+    super();
+    this.state = {
+      showUsers: true,
+      more: 'test',
+    };
+  }
+
+  // 2. 페이지 렌더링시 필요한 함수 선언
+  toggleUsersHandler(){
+    this.setState((curState) => {
+      return{showUsers : !curState.showUsers}
+    })
+  }
+
+  render() {
+    // 1. 클래스 컴포넌트내에서 페이지 렌더링
+    return (
+        <div className={classes.users}>
+          <button onClick={this.toggleUsersHandler.bind(this)}>
+            {this.showUsers ? 'Hide' : 'Show'} Users
+          </button>
+          {this.state.showUsers && usersList}
+        </div>
+    )
+  }
+}
 
 const Users = () => {
   const [showUsers, setShowUsers] = useState(true);
